3cfe7c79dea69667a2d7ff1896eaa6eaf341fbb1
brunop6 <peresbruno6@gmail.com>
2022-09-19 20:52:48 -0300

    Modo Escuro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  switchTheme(){
+    let dark = document.querySelector('#btn-dark') as HTMLInputElement;
+    let app = document.querySelector('#app') as HTMLElement;
+    let img = document.querySelector('#img-dark') as HTMLImageElement;
+
+    app.classList.toggle("dark-mode");
+
+    if(dark.checked){
+      img.src = "./../assets/sunny-outline.svg";
+    } else {
+      img.src = "./../assets/moon-outline.svg";
+    }
+  }
 }
+
